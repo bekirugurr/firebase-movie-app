8fb1787c76bb4ae13e92663f5a4c1908a261b405
bekirugurr <bekirugur070@gmail.com>
2022-04-19 15:54:56 +0300

    v3

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 import { AuthContext } from "../context/AuthContext";
 import { toastWarnNotify } from "../helpers/ToastNotify";
